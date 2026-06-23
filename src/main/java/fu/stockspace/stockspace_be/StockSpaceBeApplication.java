3abbad2a6174ba1d7d198ebd3d10803a7efd514b
Vinh Le <vinhpq.dt.468@gmail.com>
2026-06-23 22:29:02 +0700

    chore: setup dependencies, environments, and basic security config

--- src/main/java/fu/stockspace/stockspace_be/StockSpaceBeApplication.java
+++ src/main/java/fu/stockspace/stockspace_be/StockSpaceBeApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.TimeZone;
 
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients
 public class StockSpaceBeApplication {
 
     @PostConstruct
