231dc6f178297a81c0c18a9e316ec5c61a829d82
Vinh Lê <162440196+vinhleechem@users.noreply.github.com>
2026-07-28 17:16:23 +0700

    feat(auth): enhance security config, JWT handling, and user authentication for guest & user sessions

--- src/main/java/fu/stockspace/stockspace_be/auth/security/SecurityConfig.java
+++ src/main/java/fu/stockspace/stockspace_be/auth/security/SecurityConfig.java
@@ -121,6 +121,7 @@ public class SecurityConfig {
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
